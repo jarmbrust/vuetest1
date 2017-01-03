1b2e0b9e1c4ca43c7d84df1a8e0f0fa5e12bb2b5
James Armbrust <james.armbrust@gmail.com>
2017-01-02 17:14:55 -0700

    started stat program

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <app-abilities></app-abilities>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
+import Abilities from './components/Abilities.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    appAbilities: Abilities
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Abilities.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <div v-for="ability in abilityText">
+       <div class="ability col-md-4 col-md-offset-1"> {{ ability }}</div>
+       <div class="score col-md-3">score</div>
+       <div class="score col-md-3">cost</div>
+     </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        abilityText: ['Str', 'Dex', 'Con', 'Int', 'Wis', 'Chr'],
+        strScore: 8,
+        dexScore: 8,
+        conScore: 8,
+        intScore: 8,
+        wisScore: 8,
+        chrScore: 8
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
