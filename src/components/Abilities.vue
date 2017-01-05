ce45a530c568b57fb0bbee66c0a818bfd4d0709d
James Armbrust <james.armbrust@gmail.com>
2017-01-05 12:30:27 -0700

    commit

--- src/components/Abilities.vue
+++ src/components/Abilities.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="row">
-    <div v-for="ability in abilityText">
-       <div class="ability col-md-4 col-md-offset-1"> {{ ability }}</div>
-       <div class="score col-md-3">score</div>
-       <div class="score col-md-3">cost</div>
+  <div class="row container">
+    <div v-for="(ability, index) in abilities">
+       <div class="ability col-md-4 col-md-offset-1"> {{ index }}: <span class="score"> {{ ability }} </span></div>
+       <div class="col-md-3">score</div>
+       <div class="col-md-3">cost</div>
      </div>
   </div>
 </template>
@@ -12,18 +12,22 @@
   export default {
     data () {
       return {
-        abilityText: ['Str', 'Dex', 'Con', 'Int', 'Wis', 'Chr'],
-        strScore: 8,
-        dexScore: 8,
-        conScore: 8,
-        intScore: 8,
-        wisScore: 8,
-        chrScore: 8
+        abilities: {
+          'Str': 8,
+          'Dex': 8,
+          'Con': 8,
+          'Int': 8,
+          'Wis': 8,
+          'Chr': 8
+        },
+        points: 27
       }
     }
   }
 </script>
 
 <style scoped>
-
+    .container {
+        border: 1px solid black;
+    }
 </style>
