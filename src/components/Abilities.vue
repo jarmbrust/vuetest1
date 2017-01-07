748556ccfcafbb13bf7b060bff2ab0ce0b4de96e
James Armbrust <james.armbrust@gmail.com>
2017-01-07 08:26:08 -0700

    trying to get dropdowns to work...

--- src/components/Abilities.vue
+++ src/components/Abilities.vue
@@ -1,9 +1,39 @@
 <template>
   <div class="row container">
-    <div v-for="(ability, index) in abilities">
-       <div class="ability col-md-4 col-md-offset-1"> {{ index }}: <span class="score"> {{ ability }} </span></div>
-       <div class="col-md-3">score</div>
-       <div class="col-md-3">cost</div>
+    <div >
+       <div class="ability col-md-4 col-md-offset-1"> 
+         <table class="table table-hover">
+           <thead>
+              <tr>
+                <th>Ability</th>
+                <th>Score</th>
+              </tr>
+           </thead>
+           <tbody>
+             <tr v-for="(ability, index) in abilities">
+               <td>{{ index }}: </td> <td>{{ ability }}</td> <td v-if="adjustAbility">
+                 <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">Adjust</button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <option v-for="(cost, index) in abilityCost" class="dropdown-item" >{{ cost }}</option>
+                  </div>
+                  </div>
+               </td>
+             </tr>
+           </tbody>
+         </table>
+         <div class="dropdown">
+  <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown trigger
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dLabel">
+    ...
+  </ul>
+</div>
+       </div>
+       <div class="col-md-3"></div>
+       <div class="col-md-3"></div>
      </div>
   </div>
 </template>
@@ -20,8 +50,21 @@
           'Wis': 8,
           'Chr': 8
         },
-        points: 27
+        points: 27,
+        abilityCost: {
+          8: 0,
+          9: 1,
+          10: 2,
+          11: 3,
+          12: 4,
+          13: 5,
+          14: 7,
+          15: 9
+        },
+        adjustAbility: true
       }
+    },
+    methods: {
     }
   }
 </script>
